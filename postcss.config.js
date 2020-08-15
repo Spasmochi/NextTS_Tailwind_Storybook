6db9b275fc52e5e985577a73dedaee8a4ef6fd63
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-08-15 19:21:17 +1000

    Updated PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          },
+        }
+      : {}),
+  },
 };
